d6a06c3afe189dd63e198025ce1b60b32478a1e0
unbounded3 <jiaenpang54@gmail.com>
2023-01-01 13:34:40 +0800

    初次提交

---
+++ src/main/java/cn/edu/guet/datasetanalysis/DatasetAnalysisApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.datasetanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasetAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/guet/datasetanalysis/DatasetAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.datasetanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasetAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
